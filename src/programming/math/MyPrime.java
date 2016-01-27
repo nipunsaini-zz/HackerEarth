3909c50a4eb3da9802954184082354a1ce00701e
Nipun Kumar <kumar.nipun@tftus.com>
2016-01-27 21:49:26 +0530

    Updated MyPrime.java
    Added Problem Statement

--- src/programming/math/MyPrime.java
+++ src/programming/math/MyPrime.java
@@ -1,3 +1,32 @@
+/*
+Problem Statement:
+
+Recently you invented a brand-new definition of prime numbers. 
+For a given set of positive integers S let's call X a prime if there are no elements in S which are divisors of X (except X itself).
+You are given a set S. Find elements in it which are prime numbers for this set.
+
+Input:
+The first line contains one integer N - size of the set S. 
+The second line contains N space-separated integers - elements of S. 
+All the numbers are pairwise different.
+
+Output:
+Output one line: elements of S which are prime numbers for this set in the order they occur in the input. 
+Separate them by whitespaces.
+
+Constraints:
+    N <= 100
+    1 <= S[i] <= 106 (1<=i<=n)
+
+Sample Input:
+5
+10 5 3 15 16
+
+Sample Output:
+ 5 3 16
+
+*/
+
 package programming.math;
 
 import java.io.BufferedReader;
